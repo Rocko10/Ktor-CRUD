18a25a576a82b76f7c583b07f0c339b45ad13b66
Marco Nuñez <marco.soulg@gmail.com>
2019-01-10 21:25:04 -0600

    Add method in the repository

--- src/repository/MonkeyGateway.kt
+++ src/repository/MonkeyGateway.kt
@@ -2,10 +2,12 @@ package com.monkeys.repository
 
 import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 
 interface MonkeyGateway {
     fun getMonkeys(): List<Monkey?>
+    fun createMonkey(newMonkey: NewMonkey): Boolean
 }
 
 data class Monkey(val id: Int, val name: String)
@@ -26,5 +28,13 @@ private fun toMonkey(row: ResultRow): Monkey {
 class MonkeyRepository : MonkeyGateway {
     override fun getMonkeys(): List<Monkey?> {
         return Monkeys.selectAll().map { toMonkey(it) }
+    }
+
+    override fun createMonkey(newMonkey: NewMonkey): Boolean {
+        val id =Monkeys.insert {
+            it[name] = newMonkey.name
+        } get Monkeys.id
+
+        return id ?: -1 >= 0
     }
 }
